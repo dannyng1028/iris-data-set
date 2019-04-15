04a03959790d3b35e2749f80a04c0a1d77c8c0dc
Danny NG Ka Wai <49540242+dannyng1028@users.noreply.github.com>
2019-04-15 15:56:19 +0900

    Update iris v2.py

--- iris v2.py
+++ iris v2.py
@@ -63,65 +63,68 @@ plt.show()
 corr_matrix = dataset.corr()
 print(corr_matrix)
 
-# Petal-length and petal-width are highly correlated. Run the algorithm again without petal-width
 # Split-out validation dataset
 array = dataset.values
-X2 = array[:,0:3]
-Y2 = array[:,4]
+X = array[:,0:3]	# Omit petal-length
+Y = array[:,4]
 validation_size = 0.20
 seed = 0
-X_train2, X_validation2, Y_train2, Y_validation2 = model_selection.train_test_split(X2, Y2, test_size=validation_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 nsplits = 10
 scoring = 'accuracy'
-models2 = []
-models2.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models2.append(('LDA', LinearDiscriminantAnalysis()))
-models2.append(('KNN', KNeighborsClassifier()))
-models2.append(('CART', DecisionTreeClassifier()))
-models2.append(('NB', GaussianNB()))
-models2.append(('SVM', SVC(gamma='auto')))
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
 # evaluate each model in turn
-results2 = []
-names2 = []
-for name, model in models2:
-	kfold2 = model_selection.KFold(n_splits=nsplits, random_state=seed)
-	cv_results2 = model_selection.cross_val_score(model, X_train2, Y_train2, cv=kfold2, scoring=scoring)
-	results2.append(cv_results2)
-	names2.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results2.mean(), cv_results2.std())
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=nsplits, random_state=seed)
+t	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
 
 # Compare Algorithms
 fig = plt.figure()
 fig.suptitle('Algorithm Comparison')
 ax = fig.add_subplot(111)
-plt.boxplot(results2)
-ax.set_xticklabels(names2)
+plt.boxplot(results)
+ax.set_xticklabels(names)
 plt.show()
 
 # Make predictions with k-nearest neighbors on validation dataset
-knn2 = KNeighborsClassifier()
-knn2.fit(X_train2, Y_train2)
-predictions2 = knn2.predict(X_validation2)
-print('Accuracy score:', accuracy_score(Y_validation2, predictions2))
-print('Confusion matrix:\n', confusion_matrix(Y_validation2, predictions2))
-print(classification_report(Y_validation2, predictions2))
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print('Accuracy score for KNN:', accuracy_score(Y_validation, predictions))
+print('Confusion matrix for KNN:\n', confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
 
 # Look for misclassified entries
-for sl, y, p in zip(X_validation2, Y_validation2, predictions2):
-  if y!=p:
-    print(sl, y, p)
+for sl, y, p in zip(X_validation, Y_validation, predictions):
+	if y!=p:
+		print(sl, y, p)
 print('\n')
 
-# Predict with logistic regression
-LR2=LogisticRegression(solver='liblinear', multi_class='ovr')
-LR2.fit(X_train2,Y_train2)
-predictions3 = LR2.predict(X_validation2)
-print('Accuracy score for LR:', accuracy_score(Y_validation2,predictions3))
-print('Confusion matrix for LR:\n', confusion_matrix(Y_validation2, predictions3))
-print(classification_report(Y_validation2, predictions3))
-
-# Look for misclassified entries
-for sl, y, p in zip(X_validation2, Y_validation2, predictions3):
-  if y!=p:
-    print(sl, y, p)
+# Make predictions on validation data set with all models
+for name, model in models:
+	m=model
+	m.fit=(X_train, Y_train)
+	predictions=m.predict(X_validation)
+	print('Accuracy score for :'.fomat(name), accuracy_score(Y_validation, predictions))
+	print('Confusion matrix for KNN:\n', confusion_matrix(Y_validation, predictions))
+	print(classification_report(Y_validation, predictions))
+	
+	# Look for misclassified entries
+	for sl, y, p in zip(X_validation, Y_validation, predictions):
+  		if y!=p:
+    			print(sl, y, p)
+	print('\n')
